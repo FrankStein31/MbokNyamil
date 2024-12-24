b1f9124bce706809a4180390ef0f50efd1b46fe5
Frankie Steinlie <frankie.intern24slides@gmail.com>
2024-12-24 09:16:58 +0700

    perbaikan tampilan home banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 <div class="bg-gray-50 min-h-screen">
-    <div class="container mx-auto px-4 py-8 lg:px-8">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8">
         <main>
             {{-- Banner Section with Enhanced Styling --}}
-            <section class="mb-12 rounded-2xl overflow-hidden shadow-lg">
+            <section class="mb-12 rounded-2xl overflow-hidden shadow-lg relative">
                 <div class="swiper-container">
                     <div class="swiper-wrapper">
                         @foreach ($banners as $banner)
@@ -15,19 +15,18 @@
                             <img 
                                 src="{{ asset('storage/' . $banner->image) }}" 
                                 alt="{{ $banner->name }}" 
-                                class="w-full h-[400px] lg:h-[600px] object-cover"
+                                class="w-full h-[300px] lg:h-[400px] object-cover"
                             >
                         </div>
                         @endforeach
-                        <div class="swiper-pagination"></div>
                     </div>
                     
                     {{-- Custom Navigation Styling --}}
-                    <div class="absolute z-10 top-1/2 transform -translate-y-1/2 w-full px-4">
-                        <div class="swiper-button-prev bg-white/50 hover:bg-white/70 rounded-full p-3 transition-all"></div>
-                        <div class="swiper-button-next bg-white/50 hover:bg-white/70 rounded-full p-3 transition-all"></div>
-                    </div>
+                    <div class="swiper-button-prev !w-10 !h-10 !left-2 bg-white/50 hover:bg-white/70 rounded-full !after:text-lg"></div>
+                    <div class="swiper-button-next !w-10 !h-10 !right-2 bg-white/50 hover:bg-white/70 rounded-full !after:text-lg"></div>
                     
+                    {{-- Pagination --}}
+                    <div class="swiper-pagination"></div>
                 </div>
             </section>
 
@@ -42,7 +41,7 @@
                     <p class="text-gray-500 mt-2">Temukan produk berkualitas dengan harga terbaik</p>
                 </div>
 
-                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     @foreach ($products as $product)
                     <div class="bg-white rounded-2xl shadow-lg overflow-hidden 
                                 transition-all duration-300 hover:shadow-xl 
@@ -51,7 +50,7 @@
                             <img 
                                 src="{{ asset('storage/' . $product->image) }}" 
                                 alt="{{ $product->name }}" 
-                                class="w-full h-56 object-contain p-4 
+                                class="w-full h-48 object-contain p-4 
                                        group-hover:scale-105 transition-transform"
                             >
                             <div class="absolute top-4 right-4 
